24c2c17cd1a1159c667a8e36fcf960b69ff0b40d
Christian Soto <christian.j.sotoparra@gmail.com>
2021-12-03 00:27:03 -0700

    Connecting to an Sqlite3 database with Python

---
+++ script1.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+#create database, connect, execute query, and name it lite.db
+def create_table():
+	conn=sqlite3.connect("lite.db")
+	cur=conn.cursor()
+	cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+	conn.commit()
+	conn.close()
+
+def insert(item, quantity,price):
+	conn=sqlite3.connect("lite.db")
+	cur=conn.cursor()
+	cur.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity,price))
+	conn.commit()
+	conn.close()
+
+def view():
+	conn=sqlite3.connect("lite.db")
+	cur=conn.cursor()
+	cur.execute("SELECT * FROM store")
+	rows=cur.fetchall()
+	conn.close()
+	return rows
+
+print(view())
